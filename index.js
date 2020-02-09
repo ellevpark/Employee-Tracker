0104ab588922e08d28a9dbc7a1b611225451bb26
Ellen Park <ellenpark781@gmail.com>
2020-02-09 17:24:05 -0500

    added functions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require ('inquirer');
 const connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
+  // Your port;
   port: 3306,
 
   // Your username
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
 
   // Your password
   password: "Bootcamp!",
-  database: "employee.DB"
+  database: "employee_DB"
 });
 
 connection.connect(function(err) {
@@ -21,8 +21,6 @@ connection.connect(function(err) {
   start(); 
 });
 
-start();
-
 function start(){
   inquirer
   .prompt ([
@@ -30,27 +28,66 @@ function start(){
       type: "list", 
       message: "What would you like to do?",
       name: "start",
-      choices: ["View all Employees", "View All Employees by Department", "View all Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+      choices: [
+      "Add Employee", 
+      "View all Employees", 
+      "Remove Employee", 
+      "Add Department", 
+      "View all Departments",
+      "View all Employees by Department", 
+      "Delete Department",
+      "Add Roles", 
+      "Delete Roles",
+      "View all Roles", 
+      "Update Employee Role", 
+      "Update Employee Manager",
+      "Exit"
+    ]
     }
   ])
   .then (function(res){
     switch (res.start){
+
+      case "Add Employee":
+      addEmployee();
+      break;
+
       case "View all Employees":
       viewAllEmployees();
       break; 
+
+      case "Remove Employee": 
+      removeEmployee(); 
+      break;
+    
+      case "Add Department": 
+      addDept(); 
+      break;
+
+      case "View all Departments":
+      viewAllDept();
+      break;
     
       case "View Employees by Department":
       viewEmployeeByDept(); 
       break; 
     
-      case "Add Employee":
-      addEmployee();
-      break;
-    
-      case "Remove Employee": 
-      removeEmployee(); 
+      case "Delete Department": 
+      deleteDept(); 
       break;
 
+      case "Add Roles": 
+      addRole(); 
+      break;
+
+      case "Delete Roles": 
+      deleteRole(); 
+      break;
+
+      case "View all Roles": 
+      viewAllRoles(); 
+      break;
+    
       case "Update Employee Role":
       updateEmployeeRole(); 
       break;
@@ -58,112 +95,174 @@ function start(){
       case "Update Employee Manager":
       updateEmployeeManager(); 
       break;
+
+      case "Exit":
+      connection.end(); 
+      break; 
     }
   })
 }
 
+
+function addEmployee() {
+console.log("Inserting a new employee.\n");
+inquirer 
+  .prompt ([ 
+    {
+      type: "input", 
+      message: "First Name?",
+      name: "firstName",
+    },
+    {
+      type: "input", 
+      message: "Last Name?",
+      name: "lastName"
+    },
+    {
+      type: "list",
+      message: "What is the employee's role?",
+      name: "role", 
+      choices: ["Sales Person", "Lead Engineer", "Lawyer"]
+    },
+    {
+      type: "input", 
+      message: "Who is their manager?",
+      name: "employeeManager"
+    }
+  ])
+  .then (function(res){
+    const query = connection.query(
+      "INSERT INTO employee SET ?", 
+      {
+        firstName: res.firstName, 
+        lastName: res.lastName,
+        role: res.role,
+        manager: res.employeeManager, 
+      },
+      function(err, res) {
+        if (err) throw err;
+        console.log( "Employee inserted!\n");
+        // Call updateProduct AFTER the INSERT completes
+        start (); 
+      }
+    );    
+  })
+}
 function viewAllEmployees() {
   console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM employee", function(err, res) {
+  connection.query("SELECT * FROM employees", 
+  function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
-    console.log(res);
-    connection.end();
+    console.table(res);
+    start();
   });
 }
 
-function addEmployee() {
-  console.log("Inserting a new employee.\n");
-  inquirer 
-    .prompt ([ 
+function removeEmployee(){
+  //remove employee
+}
+
+function addDept(){
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "deptName", 
+      message: "What Department would you like to add?"
+    }
+  ])
+  .then(function(res){
+    console.log(res);
+    const query = connection.query(
+      "INSERT INTO departments SET ?", 
       {
-        type: "input", 
-        message: "First Name?",
-        name: "firstName",
-      },
-      {
-        type: "input", 
-        message: "Last Name?",
-        name: "lastName"
-      },
-      {
-        type: "list",
-        message: "What is the employee's role?",
-        name: "role", 
-        choices: ["Sales Person", "Lead Engineer", "Lawyer"]
-      },
-      {
-        type: "input", 
-        message: "Who is their manager?",
-        name: "employeeManager"
+        name: res.deptName
+      }, 
+      function(err, res){
+        connection.query("SELECT * FRO departments", function(err, res){
+          console.table(res); 
+          start(); 
+        })
       }
-    ])
-    .then (function(res){
-      const query = connection.query(
-        "INSERT INTO employee SET ?", 
-        {
-          firstName: res.firstName, 
-          lastName: res.lastName,
-          role: res.role,
-          manager: res.employeeManager, 
-        },
-        function(err, res) {
-          if (err) throw err;
-          console.log( "Employee inserted!\n");
-          // Call updateProduct AFTER the INSERT completes
-          start (); 
-        }
-      );    
-    })
-  }
+    )
+  })
+}
+
+function viewAllDept(){
+  // view dept
+}
 
 function viewEmployeeByDept(){
+  let departments= [];
+  connection.query ("SELECT name FROM departments", function(err, res){
+    console.log (res); 
+    dept.forEach(element => {
+      departments.push(res[i].name); 
+    });
+  })
   inquirer
   .prompt ({
     name: "viewDept", 
     type: "list", 
     message: "What department would you like to view?",
-    choices: [
-      "Sales",
-      "Engineering", 
-      "Legal"
-    ]
+    choices: departments 
   })
   .then(function(res){
-    switch(res.viewDept){
-      case "View Sales Employees":
-      viewSalesEmployees(); 
-      break;
-
-      case "View Engineering Employees":
-      viewEngineeringEmployees(); 
-      break;
-
-      case "View Legal Employees":
-      viewLegalEmployees(); 
-      break;
+  console.log (res.deptName)
+  connection.query ("SELECT id FROM departments WHERE name = `${res.deptName}`"), 
+  function(err,res){
+    connection.query("SELECT * FROM employees, roles.title, roles.salary, departments.name FROM ((roles INNER JOIN departments ON departments.id = roles.department_id) INNER JOIN employees ON roles.id = employees.role_id) WHERE department_id = `${res[0].id} "),
+    function(err, res){
+      console.table (res); 
+      start();
     }
-
+   }
   })
 }
-function viewSalesEmployees(){
+function deleteDept (){
   
 }
-
-function deleteEmployee() {
-  console.log("Deleting employee...\n");
-  connection.query(
-    "DELETE FROM employee WHERE ?",
+function addRole() {
+inquirer
+.prompt([
+  {
+    type: "input", 
+    name: "role",
+    message: "What role would you like to add?"
+  },
+  {
+    type: "input",
+    name: "salary",
+    message: "What is the salary for the role?"
+  }
+])
+.then (function(res){
+  console.log(res); 
+  const query = connection.query(
+    "INSERT INTO roles SET ?",
     {
-      first_name: "Ellen"
-    },
-    function(err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " person deleted!\n");
-      // Call readProducts AFTER the DELETE completes
-      readEmployee();
+      role: res.role,
+      salary: res.salary 
+    }, 
+    function (err, res){
+      const id = res.insertId;
+      updateRoleDepartment(id)
     }
-  );
+  )
+})
+}
+function updateRoleDepartment(){
+
+}
+function viewAllRoles(){
+
+}
+function updateEmployeeRole(){
+
+}
+function updateEmployeeManager(){
+
 }
 
 
